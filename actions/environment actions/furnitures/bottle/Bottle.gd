7d182b00c67186245270f7abb6e951e0e33fa985
Eugene Kang <44959850+eug2ne@users.noreply.github.com>
2025-08-21 22:47:55 +0200

    need adjustments to calculating trajectory

--- actions/environment actions/furnitures/bottle/Bottle.gd
+++ actions/environment actions/furnitures/bottle/Bottle.gd
@@ -1,12 +1,25 @@
 extends Action
 
-@export var SPEED: float = 70
-@export var REACH: float = 17.5
-@onready var character_body: CharacterBody2D = $Area2D/CharacterBody2D
-@onready var raycast: RayCast2D = $Area2D/CharacterBody2D/RayCast2D
+# throw physics configs
+var SPEED: float
+@export var REACH: float = 30
+const GRAVITY: float = 1
+@onready var DRAG: float = ProjectSettings.get_setting("physics/2d/default_linear_damp")
+var TIMESTAMP: float = 0.0
+var target: Character
+var initial_position: Vector2
+var throw_direction: Vector2
+var throw_angle: float
+var z_axis: float = 0.0
+var activated: bool = false
+
+@onready var character_body: CharacterBody2D = $CharacterBody2D
+@onready var raycast: RayCast2D = $CharacterBody2D/RayCast2D
 
 
 func _ready():
+	# assign target_area
+	target_area = $CharacterBody2D/Area2D
 	# disable raycast
 	raycast.enabled = false
 
@@ -21,28 +34,65 @@ func sort_by_distance_to_player(target1, target2):
 	else:
 		return target1_distance < target2_distance
 
-func _throw():
+func get_target() -> Character:
 	# get target
 	var targets: Array[Node] = get_tree().get_nodes_in_group(target_group)
 	targets.sort_custom(sort_by_distance_to_player)
 	var target: Character = targets[0]
 	
-	# move toward target
-	var direction: Vector2 = target_area.global_position.direction_to(target.global_position).normalized()
-	character_body.velocity = direction * SPEED
-	# enable raycast + set raycast direction
-	raycast.target_position = direction * REACH
-	raycast.enabled = true
-	
-	print(parent)
-	print("throw")
+	return target
 
-func _physics_process(_delta: float) -> void:
+func get_trajectory(initial_pos: Vector2, direction: Vector2, desired_distance: float, desired_angle: float) -> void:
+	# TODO: need to adjust trajectory (more curve toward target)
+	initial_position = initial_pos
+	throw_direction = direction.normalized()
+	throw_angle = desired_angle + 30
+	
+	SPEED = pow(abs(desired_distance * GRAVITY / sin(2 * deg_to_rad(desired_angle))), 0.5)
+	
+	character_body.global_position = initial_position
+	TIMESTAMP = 0.0
+
+func _throw():
+	# activate bottle
+	activated = true
+	
+	# get target + direction + angle
+	target = get_target()
+	var direction: Vector2 = character_body.global_position.direction_to(target.global_position).normalized()
+	var distance: float = character_body.global_position.distance_to(target.global_position)
+	var angle: float = direction.angle()
+	get_trajectory(character_body.global_position, direction, distance, angle)
+	
+	# enable raycast
+	raycast.enabled = true
+
+func _physics_process(delta: float) -> void:
+	if !activated:
+		return
+	
 	# TODO: move toward target in parabola trajectory
-	character_body.move_and_slide()
+	TIMESTAMP += delta
+	
+	z_axis = SPEED * sin(deg_to_rad(throw_angle)) * TIMESTAMP - 0.5 * GRAVITY
+	if abs(z_axis) > 0:
+		var x_axis: float = SPEED * cos(deg_to_rad(throw_angle)) * TIMESTAMP
+		character_body.global_position = initial_position + throw_direction * x_axis
+		# set raycast direction
+		raycast.target_position = character_body.velocity.normalized() * REACH
 	
 	# detect hit from target using raycast
-	if raycast.is_colliding():
-		character_body.velocity = character_body.velocity / 2
-		raycast.enabled = false
-		# TODO: break bottle (hit targets within target_area)
+	#if raycast.is_colliding():
+		#print("raycast colliding")
+		#if character_body.global_position.distance_to(target.global_position) < 30:
+			#print("hit!!!!")
+			#character_body.velocity = Vector2(0,0)
+			#activated = false
+			## TODO: break bottle (hit targets within target_area)
+			## FIXME: target_area pushes bottle away
+			##target_area.global_position = character_body.global_position
+	#else:
+		#print("adjust bottle direction")
+		## TODO: adjust direction to closest new target
+	
+	character_body.move_and_slide()
--- interaction/spawn areas/bottle_spawn_area.gd
+++ interaction/spawn areas/bottle_spawn_area.gd
@@ -9,7 +9,7 @@ func _ready():
 func _add_bottle(new_bottle: Action):
 	add_child(new_bottle)
 	bottle = new_bottle
-	bottle.target_area.global_position = global_position
+	bottle.character_body.global_position = global_position
 
 func _throw_bottle():
 	# set bottle parent to current_pc
